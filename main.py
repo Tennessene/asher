45b3a5578223760e26edd6d9f5577ecadb0b427c
Anston Sorensen <ansorensen1118@gmail.com>
2024-01-26 21:33:07 -0600

    Update timing systems and treadmill variables
    Updated the timing system, added a timeout system, and updated the treadmill variables to the actual ones for my treadmill

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from time import sleep
 belt_button = Button(2)
 
 # 15 is the length of my belt in feet
-belt_length = 15
+belt_length = 8.0416666666667
+# My lowest treadmill speed takes about 40 seconds to pass
+timeout = 40
 
 
 def display(original_time=None, total_distance=None, current_speed=None, prev_rev_time=None):
@@ -16,7 +18,7 @@ def display(original_time=None, total_distance=None, current_speed=None, prev_re
     print('Current Workout Information')
     print('')
     if original_time is None:
-        print(f'Time elapsed: --:--:--')
+        print(f'Time Elapsed: 00:00:00')
         print('')
         print('Distance')
         print(f'Miles: --')
@@ -27,10 +29,28 @@ def display(original_time=None, total_distance=None, current_speed=None, prev_re
         print(f'KM/H: --')
         print('')
         print('Pace')
-        print(f'Mile: --')
-        print(f'Kilometer: --')
+        print(f'Mile: --:--')
+        print(f'Kilometer: --:--')
+    elif current_speed == 0:
+        seconds_elapsed = time.time() - original_time
+        time_elapsed = increment(seconds_elapsed)
+        mi = total_distance / 5280
+        km = total_distance / 3280.84
+        print(f'Time Elapsed: {time_elapsed}')
+        print('')
+        print('Distance')
+        print(f'Miles: {round(mi, 2)}')
+        print(f'Kilometers: {round(km, 2)}')
+        print('')
+        print('Current Speed')
+        print(f'M/H: 0')
+        print(f'KM/H: 0')
+        print('')
+        print('Pace')
+        print(f'Mile: 00:00')
+        print(f'Kilometer: 00:00')
     else:
-        seconds_elapsed = prev_rev_time - original_time
+        seconds_elapsed = time.time() - original_time
         time_elapsed = increment(seconds_elapsed)
         mi = total_distance / 5280
         km = total_distance / 3280.84
@@ -46,7 +66,7 @@ def display(original_time=None, total_distance=None, current_speed=None, prev_re
         km_pace_sec = round((min_km * 60) % 60)
         if km_pace_sec < 10:
             km_pace_sec = f'0{km_pace_sec}'
-        print(f'Time elapsed: {time_elapsed}')
+        print(f'Time Elapsed: {time_elapsed}')
         print('')
         print('Distance')
         print(f'Miles: {round(mi, 2)}')
@@ -57,12 +77,8 @@ def display(original_time=None, total_distance=None, current_speed=None, prev_re
         print(f'KM/H: {round(kmph, 2)}')
         print('')
         print('Pace')
-        if current_speed == 0:
-            print(f'Mile: 0')
-            print(f'Kilometer: 0')
-        else:
-            print(f'Mile: {mi_pace_min}:{mi_pace_sec}')
-            print(f'Kilometer: {km_pace_min}:{km_pace_sec}')
+        print(f'Mile: {mi_pace_min}:{mi_pace_sec}')
+        print(f'Kilometer: {km_pace_min}:{km_pace_sec}')
 
 
 def increment(seconds_elapsed):
@@ -80,43 +96,51 @@ def increment(seconds_elapsed):
 
 
 def rev_counter():
-    revolutions = 0
-    prev_rev_time = 0
     original_time = time.time()
+    prev_rev_time = original_time
+    revolutions = 0
     try:
+        display()
         while True:
+            # Times out after 40 seconds
+            # Belt should be stopped if it's taking that long
+            belt_button.wait_for_press(timeout)
+            # Code when debugging
+            # sleep(1)
+            rev_seconds = time.time() - prev_rev_time
             prev_rev_time = time.time()
+            if rev_seconds < timeout:
+                revolutions += 1
+            thread = Thread(target=update_display, args=(original_time, prev_rev_time, rev_seconds, revolutions))
+            thread.start()    
             sleep(0.25)
-            belt_button.wait_for_press()
-            rev_time = time.time()
-            revolutions += 1
-            thread = Thread(target=update_display, args=(original_time, prev_rev_time, rev_time, revolutions))
-            thread.start()
     except KeyboardInterrupt:
-        os.system('clear')
         total_distance = belt_length * revolutions
         results(original_time, prev_rev_time, total_distance)
 
 
-def update_display(original_time, prev_rev_time, rev_time, revolutions):
-    # feet * rev count
+def update_display(original_time, prev_rev_time, rev_seconds, revolutions):
+    # Feet * rev count
     # 15 = Length of my treadmill in feet
     total_distance = belt_length * revolutions
-    rev_seconds = rev_time - prev_rev_time
-    # feet / second
-    current_speed = belt_length / rev_seconds
+    if rev_seconds >= timeout:
+        # Set current speed to zero if timed out
+        current_speed = 0
+    else:
+        # Feet / second
+        current_speed = belt_length / rev_seconds
     display(original_time, total_distance, current_speed, prev_rev_time)
 
 
 def results(original_time, prev_rev_time, total_distance):
     try:
         os.system('clear')
-        seconds_elapsed = prev_rev_time - original_time
+        seconds_elapsed = time.time() - original_time
         time_elapsed = increment(seconds_elapsed)
         print('Workout Results')
         print('')
         if total_distance == 0:
-            print(f'Total time: {time_elapsed}')
+            print(f'Total Time: {time_elapsed}')
             print('')
             print('Distance')
             print(f'Miles: 0')
@@ -130,7 +154,8 @@ def results(original_time, prev_rev_time, total_distance):
             print(f'Mile: 00:00')
             print(f'Kilometer: 00:00')
         else:
-            average_speed = total_distance / seconds_elapsed
+            working_seconds_elapsed = prev_rev_time - original_time
+            average_speed = total_distance / working_seconds_elapsed
             mi = total_distance / 5280
             km = total_distance / 3280.84
             mph = average_speed / 1.46667
@@ -145,7 +170,7 @@ def results(original_time, prev_rev_time, total_distance):
             km_pace_sec = round((min_km * 60) % 60)
             if km_pace_sec < 10:
                 km_pace_sec = f'0{km_pace_sec}'
-            print(f'Total time: {time_elapsed}')
+            print(f'Total Time: {time_elapsed}')
             print('')
             print('Distance')
             print(f'Miles: {round(mi, 2)}')
@@ -172,7 +197,6 @@ def main():
             print('ASHER Treadmill Workout Program')
             print('')
             input('Start Workout ')
-            display()
             rev_counter()
         except KeyboardInterrupt:
             print('\n')
